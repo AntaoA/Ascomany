2a0f5fa127fc66bd0ce3fc6ef0a6a67aa72a024c
AntaoA <amoryantao@net-c.com>
2025-04-06 22:55:35 +0200

    3.4.3 : ajout console log

--- Flowmodoro/flowmodoro.php
+++ Flowmodoro/flowmodoro.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Flowmodoro
 Description: Timer Flowmodoro
-Version: 3.4.2
+Version: 3.4.3
 Author: Ascomany
 */
 
--- Flowmodoro/includes/shortcode-history.php
+++ Flowmodoro/includes/shortcode-history.php
@@ -692,7 +692,12 @@ document.addEventListener('DOMContentLoaded', function () {
                     setTimeout(() => {
                         const days = document.querySelectorAll('.litepicker-day');
                         const activeDates = getActiveDates(allHistory);
-
+                        // test/debug
+                        console.log("→ Actives : ", getActiveDates(allHistory));
+                        console.log("→ Days visibles : ", [...days].map(d => ({
+                            time: d.dataset.time,
+                            asDate: new Date(parseInt(d.dataset.time)).toISOString()
+                        })));
                         days.forEach(day => {
                             const ts = parseInt(day.dataset.time);
                             const dateObj = new Date(ts < 1e12 ? ts * 1000 : ts);
